836cba2af0fd60bffe76295aa899fa73ffd8e19c
hans muller <hanrikos@gmail.com>
2018-12-12 13:41:10 +0200

    exercise2 is done

---
+++ sessions/session-1/CreateAVpc.py
@@ -0,0 +1,69 @@
+#*** Create a VPC ****
+#
+#- Write a script that gets 3 args: region, cidr, tenancy
+#
+#Example for parsing args function:
+#
+#hints/vpc/example_parse_args
+#
+#- Create the vpc with CIDR 10.21.0.0/16, in us-east-1 and tenancy default
+#
+#Example for creating a vpc:
+#hints/vpc/example_create_vpc
+#
+#
+#If you finished:
+#
+#- Use the method wait_until_available upon the vpc resource to wait until the vpc reached available state
+#
+#Example for setting the wait:
+#
+#hints/vpc/example_wait_for_vpc
+#
+#- Tag the VPC with name: opsschool-1
+#
+#No hints... Use boto documentation...
+#
+#- Run pylint on your code and fix the finding (pylint yourcode.py)
+#If it's not already installed, here is a reference: https://www.pylint.org/
+
+import argparse
+import boto3
+
+
+EC2 = boto3.resource('ec2')
+EC2_CLIENT = boto3.client('ec2')
+
+def args_parser():
+    """ validate arguments and return them """
+    parser = argparse.ArgumentParser(add_help=True, description="VPC Arguments")
+    parser.add_argument("--region", "-r", help="Get target region",
+                        required=True)
+    parser.add_argument("--cidr", "-c", help="Get cidr",
+                        required=True)
+    parser.add_argument("--tenancy", "-t", help="Get tenancy",
+                        required=True)
+    return parser.parse_args()
+
+
+def create_vpc(cidr, tenancy, region):
+    """ Return VPC description after creation """
+    response = EC2.create_vpc(CidrBlock=cidr, InstanceTenancy=tenancy)
+    return response
+
+EC2
+def main(args):
+    print(args.cidr)
+    vpc = create_vpc(args.cidr, args.tenancy, args.region)
+    print(f"VPC is created with ID:{vpc.id} \nCIDR Block: {vpc.cidr_block}")
+
+    response = EC2_CLIENT.describe_vpcs()
+    i = 0
+    while i < len(response['Vpcs']):
+        print(f"VpcId: {response['Vpcs'][i]['VpcId']}")
+        i += 1
+
+
+if __name__ == "__main__":
+    args = args_parser()
+    main(args)
---
+++ sessions/session-1/decorate_existing_vpc.py
@@ -0,0 +1,95 @@
+import argparse
+import boto3
+
+
+EC2 = boto3.resource('ec2')
+EC2_CLIENT = boto3.client('ec2')
+
+def args_parser():
+    """ validate arguments and return them """
+    parser = argparse.ArgumentParser(add_help=True, description="VPC Arguments")
+    parser.add_argument("--region", "-r", help="Get target region",
+                        required=True)
+    return parser.parse_args()
+
+
+def create_vpc(cidr, tenancy, region):
+    """ Return VPC description after creation """
+    response = EC2.create_vpc(CidrBlock=cidr, InstanceTenancy=tenancy)
+    return response
+
+
+def list_existing_vpcs():
+    vpc_ids=[]
+    try:
+        response = EC2_CLIENT.describe_vpcs()
+        i = 0
+        while i < len(response['Vpcs']):
+            print(f"List of VPCs: \nVpcId: {response['Vpcs'][i]['VpcId']}")
+            vpc_ids.append(response['Vpcs'][i]['VpcId'])
+            i += 1
+    except Exception as e:
+        print(e)
+
+    return vpc_ids
+
+
+def select_vpc(vpc_ids):
+    """ Retrun a selected vpc id chosen by user """
+    print("Select vpc to act on:")
+    for vpc_id in vpc_ids:
+        print(vpc_id)
+    id_selected = input("Enter id: ")
+    return id_selected
+
+
+def create_internet_gateway():
+    """ Return description of created internet gateway """
+    response = EC2.create_internet_gateway()
+    return response
+
+
+def attach_internet_getaway(selected_vpc, ig):
+    try:
+        ig.attach_to_vpc(VpcId=selected_vpc)
+        print(ig.id)
+    except Exception as e:
+        print(e)
+
+
+def create_dhcp_options():
+    """ Return description of created dhcp options """
+    response = EC2.create_dhcp_options(
+        DhcpConfigurations=[
+            {
+                'Key': 'domain-name-servers',
+                'Values': [
+                    '10.21.0.10',
+                    '10.21.2.10',
+                ],
+            },
+        ],
+    )
+    return response
+
+
+def attach_dhcp_to_vpc(selected_vpc, dhcp_options):
+    """ Return description of created dhcp options """
+    response = dhcp_options.associate_with_vpc(
+        VpcId=selected_vpc
+    )
+    return response
+
+
+def main(args):
+    vpc_ids = list_existing_vpcs()
+    selected_vpc = select_vpc(vpc_ids)
+    ig = create_internet_gateway()
+    attach_internet_getaway(selected_vpc, ig)
+    dhcp_options = create_dhcp_options()
+    attach_dhcp_to_vpc(selected_vpc, dhcp_options)
+
+
+if __name__ == "__main__":
+    args = args_parser()
+    main(args)
